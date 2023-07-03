1c10075f60e8b26ec72f0b60020be7a1a46b531f
Tawsif Haque <68259102+Iftee97@users.noreply.github.com>
2023-07-03 16:37:43 +0600

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -104,11 +104,22 @@ const logoutUser = (req, res) => {
 // @route   GET /api/users/profile
 // @access  Private
 const getUserProfile = asyncHandler(async (req, res) => {
-  if (req.user) {
-    res.status(200).json({
-      _id: req.user._id,
-      name: req.user.name,
-      email: req.user.email,
+  // if (req.user) {
+  //   res.status(200).json({
+  //     _id: req.user._id,
+  //     name: req.user.name,
+  //     email: req.user.email,
+  //   });
+  // } else {
+  //   res.status(404);
+  //   throw new Error('User not found');
+  // }
+  const user = await User.findById(req.user._id);
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
     });
   } else {
     res.status(404);
