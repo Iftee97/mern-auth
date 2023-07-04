62c3b4a1e7516e3051f85e8e298bfedf47ed83ef
Iftee97 <tawsifmasrur@gmail.com>
2023-07-05 01:27:17 +0600

    ...

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,23 +3,26 @@ import asyncHandler from 'express-async-handler';
 import User from '../models/userModel.js';
 
 const protect = asyncHandler(async (req, res, next) => {
-  let token;
-  token = req.cookies.jwt;
-  console.log('token from middleware: >>>>>>>>', token);
-  if (token) {
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = await User.findById(decoded.userId).select('-password'); // exclude password from user object
-      next();
-    } catch (error) {
-      console.error(error);
-      res.status(401);
-      throw new Error('Not authorized, invalid token');
-    }
-  } else {
-    res.status(401);
-    throw new Error('Not authorized, no token');
-  }
+  // let token;
+  // token = req.cookies.jwt;
+  // console.log('token from middleware: >>>>>>>>', token);
+  // if (token) {
+  //   try {
+  //     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+  //     req.user = await User.findById(decoded.userId).select('-password'); // exclude password from user object
+  //     next();
+  //   } catch (error) {
+  //     console.error(error);
+  //     res.status(401);
+  //     throw new Error('Not authorized, invalid token');
+  //   }
+  // } else {
+  //   res.status(401);
+  //   throw new Error('Not authorized, no token');
+  // }
+
+  const { authorization } = req.headers;
+  console.log('authorization: >>>>>>>>', authorization);
 });
 
 export {
