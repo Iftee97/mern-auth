95b4c4a62ce3a8df3fd7ad1c70e530b1a9c49177
Iftee97 <tawsifmasrur@gmail.com>
2023-07-05 02:06:40 +0600

    ...

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -5,7 +5,7 @@ import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react'
 const baseQuery = fetchBaseQuery({
   baseUrl: 'https://mern-auth-production-5596.up.railway.app',
   prepareHeaders: (headers, { getState }) => {
-    const token = getState().auth.token
+    const token = getState().token
     if (token) {
       headers.set('authorization', `Bearer ${token}`)
     }
