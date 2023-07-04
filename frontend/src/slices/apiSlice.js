162c64cab0c75affddb24f3c9f46d1372f6835d4
Iftee97 <tawsifmasrur@gmail.com>
2023-07-05 01:44:00 +0600

    ...

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -2,7 +2,16 @@ import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react'
 
 // keep empty string '' in dev because we're using a proxy in vite config
 // but use baeUrl: 'https://mern-auth-production-5596.up.railway.app' in prod
-const baseQuery = fetchBaseQuery({ baseUrl: 'https://mern-auth-production-5596.up.railway.app' })
+const baseQuery = fetchBaseQuery({
+  baseUrl: 'https://mern-auth-production-5596.up.railway.app',
+  prepareHeaders: (headers, { getState }) => {
+    const token = getState().auth.token
+    if (token) {
+      headers.set('authorization', `Bearer ${token}`)
+    }
+    return headers
+  }
+})
 
 export const apiSlice = createApi({
   reducerPath: "api",
