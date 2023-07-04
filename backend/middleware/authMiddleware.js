bd3de33227bb1c37f6c8c31e30a2dac25741001a
Iftee97 <tawsifmasrur@gmail.com>
2023-07-05 02:21:42 +0600

    ...

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -21,9 +21,22 @@ const protect = asyncHandler(async (req, res, next) => {
   //   throw new Error('Not authorized, no token');
   // }
 
-  const token = req.cookies;
   const { authorization } = req.headers;
-  console.log({ token, authorization });
+  console.log('authorization: >>>>>>>>', authorization);
+  if (!authorization) {
+    return res.status(401).json({ error: 'authorization token required' })
+  }
+  const token = authorization.split(' ')[1];
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.userId).select('-password'); // exclude password from user object
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401).json({ error: "request is not authorized" })
+    }
+  }
 });
 
 export {
